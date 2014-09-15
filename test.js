e26caa3bfff6bf4544a8836179bdf33e9650a582
Jeremy <sgzxzj@gmail.com>
2014-09-15 14:43:54 +0800

    基本完成随见聊天功能，切换也不会重复和之前的人相连……

--- test.js
+++ test.js
@@ -16,9 +16,9 @@ app.get('/', function (req, res) {
 var pair_id = 0;
 
 
-var socket_array = [];
+var wait_array = [];   //存放等待列表的socket
 
-var pair_array = [];
+var pair_array = [];  //存放pair对象
 
 //io 链接上
 io.sockets.on('connection', function (socket) {
@@ -31,38 +31,58 @@ io.sockets.on('connection', function (socket) {
     socket.on('name', function (data) {
         socket.name = data;
         socket.emit("msg", data + " 欢迎你来到easy chat");
+    });
 
-
-        //todo 关于配对的操作
-        //todo 如果没有就把这个推进去，并返回一个等待的消息
-        if (socket_array.length == 0) {
-            socket_array.push(socket);
-            socket.emit("msg", "请耐心等待正在配对中……");
+    //接受一个对方已经准备好要开始聊天的消息
+    socket.on("is_ready", function (data) {
+        if (data == true) {
+            //将这个socket 推入wait_array
+            wait_array.push(socket);
+            socket.emit("msg", "正在等待配对中……");
+            create_pair(jude_pair());
         }
-        //todo 如果有的话，就在socket_array里面选一个出来跟它配对
+    });
+
+    //接受一个切换的消息
+    socket.on("change", function (data) {
+        //如果没有配对
+        if (socket.pair_id == -1 || socket.pair_id == undefined
+            || socket.pair_id == null) {
+            //暂时不做任何处理
+        }
+
+        //有配对
         else {
-            //生成一个pair
-            var pair = {"a": socket_array[0], "b": socket, pair_id: pair_id};
-            socket_array[0].pair_id = pair_id;
-            socket.pair_id = pair_id;
-            pair_id++;
+            for (var i = 0; i < pair_array.length; i++) {
+                if (socket.pair_id == pair_array[i].pair_id) {
+                    var another_socket;
+                    if (socket == pair_array[i].a)
+                        another_socket = pair_array[i].b;
+                    else if (socket == pair_array[i].b)
+                        another_socket = pair_array[i].a;
 
-            //删除等待数组里面的元素
-            socket_array.splice(0, 1);
+                    pair_array.splice(i, 1);//将这个pair从数组中删除掉
 
-            //推送进入数组
-            pair_array.push(pair);
+                    //对被动切换的客服端的操作
+                    another_socket.pair_id = -1;
+                    wait_array.unshift(another_socket); //将另一个socket插入等待池的头部
+                    another_socket.emit("is_pair", false);
+                    another_socket.emit("msg", ":操……" + socket.name + "+选择了切换……");
+                    another_socket.emit("msg", "正在为您重新配对，请耐心等待……");
 
-            //发送消息
-            pair.a.emit("msg", "与" + socket.name + "配对成功，尽情聊天吧……");
-            pair.b.emit("msg", "与" + pair.a.name + "配对成功，尽情聊天吧……");
+                    //对主动切换的客服端的操作
+                    socket.pair_id = -1;
+                    wait_array.push(socket); //将另一个socket插入等待池的尾部
+                    socket.emit("is_pair", false);
+                    socket.emit("msg", "正在为您重新配对，请耐心等待……");
 
+                    //进行重新配对
+                    create_pair(jude_pair());
+                    break;
+                }
+
+            }
         }
-
-
-
-
-
 
 
     });
@@ -70,14 +90,16 @@ io.sockets.on('connection', function (socket) {
     //接受客户端发送过来的聊天信息,再群发出去
     socket.on('msg', function (data) {
 
+        console.log(socket.pair_id);
+
         //如果是没有配对的用户，就给一个提示
         if (socket.pair_id == -1 || socket.pair_id == undefined
             || socket.pair_id == null) {
             socket.emit("msg", "对不起……还没有人跟你配对……");
         }
 
-
-        //找到需要转发的用户，把消息转发出去
+        // todo 这个地方存在重大效率问题，必须必须要改,room
+        // todo 找到需要转发的用户，把消息转发出去
         for (var i = 0; i < pair_array.length; i++) {
             if (socket.pair_id == pair_array[i].pair_id) {
                 pair_array[i].a.emit("msg", socket.name + ":" + data);
@@ -89,17 +111,14 @@ io.sockets.on('connection', function (socket) {
 
     //与socket失去联系的触发的函数
     socket.on('disconnect', function () {
-        //console.log('操你妈 失联了。。。。。。');
-        //todo 当有一个用户断开的时候
-        //todo 判断是否配对
 
         //todo 如果没有配对
         if (socket.pair_id == -1 || socket.pair_id == undefined
             || socket.pair_id == null) {
             //暂时不做任何处理
-            for (var i = 0; i < socket_array.length; i++) {
-                if (socket_array[i] == socket) {
-                    socket_array.splice(i, 1);
+            for (var i = 0; i < wait_array.length; i++) {
+                if (wait_array[i] == socket) {
+                    wait_array.splice(i, 1);
                     break;
                 }
 
@@ -111,52 +130,71 @@ io.sockets.on('connection', function (socket) {
             //从配对的数组中找出来，发一个消息，再把这个pair删掉
             for (var i = 0; i < pair_array.length; i++) {
                 if (socket.pair_id == pair_array[i].pair_id) {
-                    if (socket == pair_array[i].a) {
-                        pair_array[i].b.pair_id = -1;
-                        socket_array.push(pair_array[i].b);
-                        pair_array[i].b.emit("msg", ":操……" + pair_array[i].a.name + "+对方下线了");
-                        pair_array[i].b.emit("msg", "正在为您重新配对，请耐心等待……");
-                    }
-                    else if (socket == pair_array[i].b) {
-                        pair_array[i].a.pair_id = -1;
-                        socket_array.push(pair_array[i].a);
-                        pair_array[i].a.emit("msg", ":操……" + pair_array[i].b.name + "+对方下线了");
-                        pair_array[i].a.emit("msg", "正在为您重新配对，请耐心等待……");
-                    }
-                    //从配对数组中删除掉
-                    pair_array.splice(i, 1);
+                    var another_socket;
+                    if (socket == pair_array[i].a)
+                        another_socket = pair_array[i].b;
+                    else if (socket == pair_array[i].b)
+                        another_socket = pair_array[i].a;
 
-                    if(socket_array.length>=2){
-                        //生成一个pair
-                        var pair = {"a": socket_array[0], "b": socket_array[1], pair_id: pair_id};
-                        socket_array[0].pair_id = pair_id;
-                        socket_array[1].pair_id = pair_id;
-                        pair_id++;
 
-                        //删除等待数组里面的元素
-                        socket_array.splice(0, 2);
+                    pair_array.splice(i, 1);//将这个pair从数组中删除掉
+                    another_socket.pair_id = -1;
+                    wait_array.unshift(another_socket); //将另一个socket插入等待池的头部
+                    another_socket.emit("is_pair", false);
+                    another_socket.emit("msg", ":操……" + socket.name + "+对方下线了");
+                    another_socket.emit("msg", "正在为您重新配对，请耐心等待……");
 
-                        //推送进入数组
-                        pair_array.push(pair);
-
-                        //发送消息
-                        pair.a.emit("msg", "与" + pair.b.name + "配对成功，尽情聊天吧……");
-                        pair.b.emit("msg", "与" + pair.a.name + "配对成功，尽情聊天吧……");
-                    }
-
+                    //进行重新配对
+                    create_pair(jude_pair());
+                    break;
                 }
 
             }
-
-
         }
 
-        console.log("socket_array----" + socket_array.length);
-        console.log("pair_array------" + pair_array.length);
     });
 
-
 });
+
+
+//todo 判断是否需要生成新的pair
+//todo 根据等待池里的socket来判断需要生成多少个pair,返回一个整数
+function jude_pair() {
+
+    var account = parseInt(wait_array.length / 2);
+
+    if (wait_array.length % 2 == 0)
+        return account - 1;
+
+    return account;
+
+}
+
+
+//todo 根据需要配对的数量进行新的配对
+function create_pair(account) {
+
+    for (var i = 0; i < account; i++) {
+        var a_socket = wait_array[i * 2];
+        var b_socket = wait_array[i * 2 + 1];
+        pair_id++;
+        var pair = {"a": a_socket, "b": b_socket, pair_id: pair_id};
+        a_socket.pair_id = pair_id;
+        b_socket.pair_id = pair_id;
+        pair_array.push(pair);
+
+        //发送消息
+        a_socket.emit("msg", "与" + b_socket.name + "配对成功，尽情聊天吧……");
+        b_socket.emit("msg", "与" + a_socket.name + "配对成功，尽情聊天吧……");
+
+        //告诉客户端配对成功
+        a_socket.emit("is_pair", true);
+        b_socket.emit("is_pair", true);
+    }
+
+    //删除等待数组里面的元素
+    wait_array.splice(0, account * 2);
+}
 
 
 http.listen(3000, function () {
